75c5822ce2788b31fbddc189fa5e946818681ac2
Ahmed <ahmedaraby01023980886@gmail.com>
2023-08-06 20:06:59 +0300

    floating icons

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ const App = () => {
     const [lengthActive, setLengthActive] = useState("short");
     return (
         close ? (
-            <div className="z-50 absolute right-5 top-5 h-10 w-10 bg-slate-700 rounded-md flex justify-center items-center text-white cursor-pointer" onClick={() => setClose(false)}>
+            <div className="z-50 fixed right-5 top-5 h-10 w-10 bg-slate-700 rounded-md flex justify-center items-center text-white cursor-pointer" onClick={() => setClose(false)}>
                 <FontAwesomeIcon icon={faGear} className='setting-gear' />
             </div>
         ) : (
-            <div className="z-50 h-screen w-full bg-slate-800 bg-opacity-25 absolute top-0">
+            <div className="z-50 h-screen w-full bg-slate-800 bg-opacity-25 fixed top-0">
                 <div className="extenstion-container flex flex-col justify-between gap-3 absolute p-5 right-0 bg-slate-800 h-screen w-2/5 lg:w-2/5 xl:w-2/6 2xl:w-1/4">
                     <Name />
                     <User setClose={setClose} />
